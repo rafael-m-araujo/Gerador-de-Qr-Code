549eb9aa8c6ac195da82a3370fe46170dd803638
Rafael Mendes Araújo <rafaelmendes0006@gmail.com>
2026-06-18 11:41:45 -0300

    Finalização do Gerador de Qr Code!

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
-console.log("teste");
+const container = document.querySelector(".container");
+
+const qrCodeBtn = document.querySelector("#qr-form button");
+
+const qrCodeInput = document.querySelector("#qr-form input");
+
+const qrCodeImg = document.querySelector("#qr-code img");
+
+// Eventos
+function generateQrCode() {
+const qrCodeInputValue = qrCodeInput.value;
+
+if (!qrCodeInputValue) return;
+
+qrCodeBtn.innerText = "Gerando código...";
+
+qrCodeImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrCodeInputValue}`;
+
+qrCodeImg.addEventListener("load", () => {
+    container.classList.add("active");
+    qrCodeBtn.innerText = "Código criado!";
+});
+}
+
+qrCodeBtn.addEventListener("click", () => {
+  generateQrCode();
+});
